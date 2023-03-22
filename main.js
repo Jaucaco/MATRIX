fc2a75b19e26ef7e6eb5d72edb1728e1509179aa
jaucaco <joakoballesteros2005@gmail.com>
2023-03-22 19:01:42 -0300

    final

--- main.js
+++ main.js
@@ -1,47 +1,44 @@
 let canvas = document.querySelector('canvas');
 let ctx = canvas.getContext('2d');
-let letters = ['N','E','O'];
+let letters = [];
 
-let letterSelect = document.querySelector('#letters');
-letterSelect.addEventListener('change', () => {
-    letters = letterSelect.value.split('');
+document.querySelector('#word-form').addEventListener('submit', function (e) {
+  e.preventDefault();
+  letters = document.querySelector('#word-input').value.split('');
+  document.querySelector('#form-container').style.display = 'none';
 });
 
 canvas.height = window.innerHeight;
 canvas.width = window.innerWidth;
-let drops = []
+let drops = [];
 
-for (let i = 0; i < canvas.width / 15; i++ ){
-    drops.push ({x:i * 15, y: Math.ceil(Math.random()* 60) * 15})
+for (let i = 0; i < canvas.width / 15; i++) {
+  drops.push({ x: i * 15, y: Math.ceil(Math.random() * 60) * 15 });
 }
 
 const drop = () => {
-    ctx.fillStyle = "rgba(0, 0, 0, 0.1)";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    
-    for (let i = 0; i < drops.length; i++) {
-        let text = letters[Math.floor(Math.random()* letters.length)];
-        if (drops[i].y > canvas.height){
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.1)';
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-            drops[i].y = -15;
-        }
-        drops[i].y += 15;
-        ctx.font = '15px Cournier Prime Code';
-        ctx.fillStyle = "#00FF40";
-        ctx.fillText (text, drops [i].x, drops[i].y);
+  for (let i = 0; i < drops.length; i++) {
+    let text = letters[Math.floor(Math.random() * letters.length)];
+    if (drops[i].y > canvas.height) {
+      drops[i].y = -15;
     }
-}
+    drops[i].y += 15;
+    ctx.font = '15px Cournier Prime Code';
+    ctx.fillStyle = '#00FF40';
+    ctx.fillText(text, drops[i].x, drops[i].y);
+  }
+};
 
 setInterval(drop, 70);
 
 window.addEventListener('resize', () => {
-
-    canvas.height = window.innerHeight;
-    canvas.width = window.innerWidth;
-    drops = []
-    for(let i = 0; i <canvas.width/ 15; i++){
-        drops.push({x: i * 15, y: Math.ceil(Math.random()* 60) * -15})
-
-    }
-
-})
+  canvas.height = window.innerHeight;
+  canvas.width = window.innerWidth;
+  drops = [];
+  for (let i = 0; i < canvas.width / 15; i++) {
+    drops.push({ x: i * 15, y: Math.ceil(Math.random() * 60) * -15 });
+  }
+});
